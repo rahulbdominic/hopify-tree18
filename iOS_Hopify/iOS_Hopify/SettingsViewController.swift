0c7f939d8714a98e1f5ddcd3c1d4eba6bc297240
bartchr808 <bart.chr@gmail.com>
2018-02-17 11:00:58 -0800

    settings view

---
+++ iOS_Hopify/iOS_Hopify/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.swift
+//  iOS_Hopify
+//
+//  Created by Bart Chrzaszcz on 2018-02-17.
+//  Copyright © 2018 Bart Chrzaszcz. All rights reserved.
+//
+
+import UIKit
+
+
+class SettingsViewController: UIViewController {
+    
+}

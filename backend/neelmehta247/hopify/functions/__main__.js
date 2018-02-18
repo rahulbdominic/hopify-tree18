9260d742927a188cdf5f01b6800c1c581a0a2723
Neel Mehta <neelmehta247@gmail.com>
2018-02-17 19:12:12 -0800

    Final integration

--- backend/neelmehta247/hopify/functions/__main__.js
+++ backend/neelmehta247/hopify/functions/__main__.js
@@ -13,18 +13,27 @@ const DISTANCE_MATRIX_URL = 'https://maps.googleapis.com/maps/api/distancematrix
 * @param {integer} maxPrice This amount is the maximum price from 0-4
 * @param {integer} hours The number of hours the user is willing to spend
 * @param {array} interests The users interests
-* @returns {array}
+* @returns {object}
 */
 module.exports = async (lat, lng, radius, maxPrice, hours, interests = [], context) => {
     const result = await lib.neelmehta247.hopify['@dev'].maps(lat, lng, radius, maxPrice, interests);
 
     const prioritized = priorities(result).slice(0, hours);
+    const vertices = placeids(prioritized);
+    const edges = pairs(vertices.length);
+    const edgeMap = await distances(prioritized);
+
+    const tps = await lib.neelmehta247.hopify['@dev'].tsp(vertices, edges, edgeMap);
+    const finalArray = tps.map(position => prioritized[parseInt(position)]);
     const docName = uuid();
 
     // Write to Firebase
-    await lib({bg: true}).neelmehta247.firebase['@dev']("data", docName.toString(), { data: prioritized });
+    await lib({bg: true}).neelmehta247.firebase['@dev']("data", docName.toString(), { data: finalArray });
 
-    return prioritized;
+    return {
+        uuid: docName,
+        data: finalArray
+    };
 };
 
 const priorities = (places = []) => {
@@ -46,51 +55,58 @@ const priorities = (places = []) => {
     return places;
 }
 
-const placeids = (places = []) => {
-  placeidlist = [];
-  for(i = 0; i < places.length; i++) {
-    placeidlist.push(places[i].place_id);
-  }
-  return placeidlist;
+const placeids = (places) => {
+    placeidlist = [];
+    for (i = 0; i < places.length; i++) {
+        placeidlist.push(places[i].place_id);
+    }
+
+    return placeidlist;
 }
 
-const pairs = (places = []) => {
-  mainlist = [];
-  for (i = 0; i < places.length; i++) {
-    for (j = 0; j < places.length; j++) {
-      mainlist.push([i, j]);
+const pairs = (length) => {
+    mainlist = [];
+    for (i = 0; i < length; i++) {
+        for (j = 0; j < length; j++) {
+            if (i !== j) {
+                mainlist.push([i, j]);
+            }
+        }
     }
-  }
-  return mainlist;
+
+    return mainlist;
 }
 
-const distances = async (places = [], hours, lat, lng) => {
-  journeys = {};
+const distances = async (places) => {
+    journeys = {};
 
-  for (i = 0; i < places.length; i++) {
-    for (j = i + 1; j < places.length; j++) {
-      iplace_id = places[i].place_id;
-      jplace_id = places[j].place_id;
-      if (!journeys.hasOwnProperty(places[i].place_id)) {
-        journeys[iplace_id] = {};
-      }
-      if (!journeys.hasOwnProperty(places[j].place_id)) {
-        journeys[jplace_id] = {};
-      }
-      await axios.get(DISTANCE_MATRIX_URL, {
-          params: {
-              origins: `place_id:${iplace_id}`,
-              destinations: `place_id:${jplace_id}`,
-              key: GOOGLE_API_KEY
-          }
-      }).then(result => {
-        data = result.data
-        // To add hours
-        journeys[iplace_id][jplace_id] = data.rows[0].elements[0].duration.value + 3600
-        journeys[jplace_id][iplace_id] = data.rows[0].elements[0].duration.value + 3600
-      });
+    for (i = 0; i < places.length; i++) {
+        for (j = i + 1; j < places.length; j++) {
+            iplace_id = places[i].place_id;
+            jplace_id = places[j].place_id;
+
+            if (!journeys.hasOwnProperty(places[i].place_id)) {
+                journeys[iplace_id] = {};
+            }
+
+            if (!journeys.hasOwnProperty(places[j].place_id)) {
+                journeys[jplace_id] = {};
+            }
+
+            const result = await axios.get(DISTANCE_MATRIX_URL, {
+                params: {
+                    origins: `place_id:${iplace_id}`,
+                    destinations: `place_id:${jplace_id}`,
+                    key: GOOGLE_API_KEY
+                }
+            });
+
+            data = result.data;
+            // To add hours
+            journeys[iplace_id][jplace_id] = data.rows[0].elements[0].duration.value + 3600;
+            journeys[jplace_id][iplace_id] = data.rows[0].elements[0].duration.value + 3600;
+        }
     }
-  }
 
-  return journeys;
+    return journeys;
 }
